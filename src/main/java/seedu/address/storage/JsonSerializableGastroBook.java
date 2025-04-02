5ff79769120de72cb50d0e2e34926341a3858c00
Jasonzhou97 <cheetahjason168@gmail.com>
2025-04-02 14:02:31 +0800

    Edit storage

--- src/main/java/seedu/address/storage/JsonSerializableGastroBook.java
+++ src/main/java/seedu/address/storage/JsonSerializableGastroBook.java
@@ -16,7 +16,7 @@ import seedu.address.model.reservation.Reservation;
 /**
  * An Immutable AddressBook that is serializable to JSON format.
  */
-@JsonRootName(value = "addressbook")
+@JsonRootName(value = "gastrobook")
 class JsonSerializableGastroBook {
 
     public static final String MESSAGE_DUPLICATE_PERSON = "Persons list contains duplicate person(s).";
